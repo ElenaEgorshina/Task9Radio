4d7437c061f06f70a4bc837e25569f90541fed41
Elena Egorshina <Egorshinae@yandex.ru>
2023-03-17 10:43:11 +0400

    Radio

---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void test() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(100);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+}
